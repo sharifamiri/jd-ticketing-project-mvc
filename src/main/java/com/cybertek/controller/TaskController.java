4891728d83ab56cb3b2e95ef50fac884acf41c4e
sharifamiri <sharif.amiri4@gmail.com>
2020-11-29 22:13:39 -0800

    Spring MVC Ticketing Project

--- src/main/java/com/cybertek/controller/TaskController.java
+++ src/main/java/com/cybertek/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.cybertek.controller;
 
 import com.cybertek.dto.ProjectDTO;
 import com.cybertek.dto.TaskDTO;
+import com.cybertek.enums.Status;
 import com.cybertek.service.ProjectService;
 import com.cybertek.service.TaskService;
 import com.cybertek.service.UserService;
@@ -9,7 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/task")
@@ -30,5 +36,24 @@ public class TaskController {
         model.addAttribute("employees", userService.findEmployees());
         model.addAttribute("tasks",taskService.findAll());
         return "/task/create";
+    }
+
+    @PostMapping("/create")
+    public String insertTask(Model model, TaskDTO task){
+
+        task.setTaskStatus(Status.OPEN);
+        task.setAssignedDate(LocalDate.now());
+        task.setId(UUID.randomUUID().getMostSignificantBits());
+        System.out.println("Auto generated ID: "+ task.getId());
+        taskService.save(task);
+        return "redirect:/task/create";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteTask(@PathVariable("id") Long id){
+
+        taskService.deleteByID(id);
+
+        return "redirect:/task/create";
     }
 }
